4c3184b7a8f26f1cca9c70562884aa1178347f72
Curvu <curvu6@gmail.com>
2023-04-25 12:10:20 +0100

    added base code for creating a user

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@
 ##  Filipe Rodrigues
 ##  Joás Silva
 
+# TODO: add boolean to consumer table (default: false)
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,6 +13,8 @@ import flask
 import logging
 import psycopg2
 import time
+
+import jwt
 
 app = flask.Flask(__name__)
 
@@ -38,114 +42,29 @@ def db_connection():
 #               ENDPOINTS DEFINITION              #
 ###################################################
 
-
+# Example endpoint default
 @app.route('/')
 def landing_page():
-  return """
-    Hello World (Python Native)!  <br/>
-    <br/>
-    Check the sources for instructions on how to use the endpoints!<br/>
-    <br/>
-    BD 2022 Team<br/>
-    <br/>
-  """
+  return (
+    """
+      Hello World!
+    """
+  )
 
-##
-## Demo GET
-##
-## Obtain all departments in JSON format
-##
-## To use it, access:
-##
-## http://localhost:8080/departments/
-##
-
-@app.route('/departments/', methods=['GET'])
-def get_all_departments():
-  logger.info('GET /departments')
-
-  conn = db_connection()
-  cur = conn.cursor()
-
-  try:
-    cur.execute('SELECT ndep, nome, local FROM dep')
-    rows = cur.fetchall()
-
-    logger.debug('GET /departments - parse')
-    Results = []
-    for row in rows:
-      logger.debug(row)
-      content = {'ndep': int(row[0]), 'nome': row[1], 'localidade': row[2]}
-      Results.append(content)  # appending to the payload to be returned
-
-    response = {'status': StatusCodes['success'], 'results': Results}
-
-  except (Exception, psycopg2.DatabaseError) as error:
-    logger.error(f'GET /departments - error: {error}')
-    response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
-
-  finally:
-    if conn is not None:
-      conn.close()
-
-  return flask.jsonify(response)
-
-
-##
-## Demo GET
-##
-## Obtain department with ndep <ndep>
-##
-## To use it, access:
-##
-## http://localhost:8080/departments/10
-##
-
-@app.route('/departments/<ndep>/', methods=['GET'])
-def get_department(ndep):
-  logger.info('GET /departments/<ndep>')
-
-  logger.debug(f'ndep: {ndep}')
-
-  conn = db_connection()
-  cur = conn.cursor()
-
-  try:
-    cur.execute('SELECT ndep, nome, local FROM dep where ndep = %s', (ndep,))
-    rows = cur.fetchall()
-
-    row = rows[0]
-
-    logger.debug('GET /departments/<ndep> - parse')
-    logger.debug(row)
-    content = {'ndep': int(row[0]), 'nome': row[1], 'localidade': row[2]}
-
-    response = {'status': StatusCodes['success'], 'results': content}
-
-  except (Exception, psycopg2.DatabaseError) as error:
-    logger.error(f'GET /departments/<ndep> - error: {error}')
-    response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
-
-  finally:
-    if conn is not None:
-      conn.close()
-
-  return flask.jsonify(response)
-
-
-##
-## Demo POST
-##
-## Add a new department in a JSON payload
-##
-## To use it, you need to use postman or curl:
-##
-## curl -X POST http://localhost:8080/departments/ -H 'Content-Type: application/json' -d '{'localidade': 'Polo II', 'ndep': 69, 'nome': 'Seguranca'}'
-##
-
-@app.route('/departments/', methods=['POST'])
-def add_departments():
-  logger.info('POST /departments')
+#* POST http://localhost:8080/dbproj/user
+#? Create a consumer
+# params: {username, password, name, address, birthdate}
+# return: user id if success
+#? Create a Artist
+# params: {username, password, name, address, birthdate, label_id, artistic_name)
+# header: admin authentication token
+# return: user id if success
+#? Login a user
+# params: {username, password}
+# return: authentication token if success
+@app.route('/dbproj/user', methods=['POST'])
+def create_user():
+  logger.info('POST /dbproj/user')
   payload = flask.request.get_json()
 
   conn = db_connection()
@@ -153,85 +72,62 @@ def add_departments():
 
   logger.debug(f'POST /departments - payload: {payload}')
 
-  # do not forget to validate every argument, e.g.,:
-  if 'ndep' not in payload:
-    response = {'status': StatusCodes['api_error'], 'results': 'ndep value not in payload'}
-    return flask.jsonify(response)
+  if 'username' not in payload:
+    return flask.jsonify({'status': StatusCodes['api_error'], 'error': 'username not provided'})
+  if 'password' not in payload:
+    return flask.jsonify({'status': StatusCodes['api_error'], 'error': 'password not provided'})
 
-  # parameterized queries, good for security and performance
-  statement = 'INSERT INTO dep (ndep, nome, local) VALUES (%s, %s, %s)'
-  values = (payload['ndep'], payload['localidade'], payload['nome'])
+  if 'name' not in payload and 'address' not in payload and 'birthdate' not in payload:
+    #TODO: Login
+    try:
+      response = flask.jsonify({'status': StatusCodes['api_error'], 'results': 'not implemented'})
+    except (Exception, psycopg2.DatabaseError) as error:
+      logger.error(str(error))
+      response = flask.jsonify({'status': StatusCodes['internal_error'], 'error': str(error)})
+    finally:
+      if conn is not None:
+        conn.close()
+      return response
+  else:
+    if 'name' not in payload:
+      return flask.jsonify({'status': StatusCodes['api_error'], 'error': 'name not provided'})
+    if 'address' not in payload:
+      return flask.jsonify({'status': StatusCodes['api_error'], 'error': 'address not provided'})
+    if 'birthdate' not in payload:
+      return flask.jsonify({'status': StatusCodes['api_error'], 'error': 'birthdate not provided'})
+    
+    if 'label_id' not in payload and 'artistic_name' not in payload:
+      #TODO: Create a consumer
+      try:
+        response = flask.jsonify({'status': StatusCodes['api_error'], 'results': 'not implemented'})
+      except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(str(error))
+        response = flask.jsonify({'status': StatusCodes['internal_error'], 'error': str(error)})
+      finally:
+        if conn is not None:
+          conn.close()
+        return response
+    else:
+      if 'label_id' not in payload:
+        return flask.jsonify({'status': StatusCodes['api_error'], 'error': 'label_id not provided'})
+      if 'artistic_name' not in payload:
+        return flask.jsonify({'status': StatusCodes['api_error'], 'error': 'artistic_name not provided'})
+      
+      #TODO: Create a artist
+      try:
+        response = flask.jsonify({'status': StatusCodes['api_error'], 'results': 'not implemented'})
+      except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(str(error))
+        response = flask.jsonify({'status': StatusCodes['internal_error'], 'error': str(error)})
+      finally:
+        if conn is not None:
+          conn.close()
+        return response
 
-  try:
-    cur.execute(statement, values)
 
-    # commit the transaction
-    conn.commit()
-    response = {'status': StatusCodes['success'], 'results': f'Inserted dep {payload["ndep"]}'}
-
-  except (Exception, psycopg2.DatabaseError) as error:
-    logger.error(f'POST /departments - error: {error}')
-    response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
-
-    # an error occurred, rollback
-    conn.rollback()
-
-  finally:
-    if conn is not None:
-      conn.close()
-
-  return flask.jsonify(response)
-
-
-##
-## Demo PUT
-##
-## Update a department based on a JSON payload
-##
-## To use it, you need to use postman or curl:
-##
-## curl -X PUT http://localhost:8080/departments/ -H 'Content-Type: application/json' -d '{'ndep': 69, 'localidade': 'Porto'}'
-##
-
-@app.route('/departments/<ndep>', methods=['PUT'])
-def update_departments(ndep):
-  logger.info('PUT /departments/<ndep>')
-  payload = flask.request.get_json()
-
-  conn = db_connection()
-  cur = conn.cursor()
-
-  logger.debug(f'PUT /departments/<ndep> - payload: {payload}')
-
-  # do not forget to validate every argument, e.g.,:
-  if 'localidade' not in payload:
-    response = {'status': StatusCodes['api_error'], 'results': 'localidade is required to update'}
-    return flask.jsonify(response)
-
-  # parameterized queries, good for security and performance
-  statement = 'UPDATE dep SET local = %s WHERE ndep = %s'
-  values = (payload['localidade'], ndep)
-
-  try:
-    res = cur.execute(statement, values)
-    response = {'status': StatusCodes['success'], 'results': f'Updated: {cur.rowcount}'}
-
-    # commit the transaction
-    conn.commit()
-
-  except (Exception, psycopg2.DatabaseError) as error:
-    logger.error(error)
-    response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
-
-    # an error occurred, rollback
-    conn.rollback()
-
-  finally:
-    if conn is not None:
-      conn.close()
-
-  return flask.jsonify(response)
-
+###################################################
+#                     MAIN                        #
+###################################################
 
 if __name__ == '__main__':
   # set up logging
