44885356e5a108662e24ab837634fbda3bd28ae7
Curvu <curvu6@gmail.com>
2023-04-25 13:42:12 +0100

    update to fix the mess - changing bigserial to bigint because bigserial is autoincrement

--- src/createTables.sql
+++ src/createTables.sql
@@ -24,27 +24,27 @@ DROP TABLE IF EXISTS credentials;
 CREATE TABLE credentials (
 	id        BIGSERIAL NOT NULL,
 	username  VARCHAR(32) NOT NULL,
-	password  VARCHAR(64) NOT NULL,
+	password  VARCHAR(128) NOT NULL,
 	PRIMARY KEY (id),
 	UNIQUE (username),
 	CHECK (length(username) > 0),
-	CHECK (length(password) > 0)
+	CHECK (length(password) > 0),
 );
 
 CREATE TABLE administrator (
-	credentials_id BIGSERIAL  NOT NULL,
-	PRIMARY KEY (credentials_id),
-	FOREIGN KEY (credentials_id) REFERENCES credentials(id)
+	id BIGINT  NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES credentials(id)
 );
 
 CREATE TABLE person (
-	credentials_id  BIGSERIAL NOT NULL,
-	name            VARCHAR(256) NOT NULL,
-	address         VARCHAR(256) NOT NULL,
-	email            VARCHAR(128) NOT NULL,
-	birthdate       DATE NOT NULL,
-	PRIMARY KEY (credentials_id),
-	FOREIGN KEY (credentials_id) REFERENCES credentials(id),
+	id        BIGINT NOT NULL,
+	name      VARCHAR(256) NOT NULL,
+	address   VARCHAR(256) NOT NULL,
+	email     VARCHAR(128) NOT NULL,
+	birthdate DATE NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES credentials(id),
 	CHECK (length(name) > 0),
 	CHECK (length(address) > 0),
 	CHECK (length(email) > 0),
@@ -52,7 +52,7 @@ CREATE TABLE person (
 );
 
 CREATE TABLE label (
-	id      BIGSERIAL NOT NULL,
+	id      BIGINT NOT NULL,
 	name    VARCHAR(128) NOT NULL,
 	contact VARCHAR(128) NOT NULL,
 	PRIMARY KEY (id),
@@ -62,25 +62,25 @@ CREATE TABLE label (
 );
 
 CREATE TABLE artist (
-	credentials_id  BIGSERIAL NOT NULL,
-	label_id        BIGSERIAL NOT NULL,
-	artistic_name   VARCHAR(128) NOT NULL,
-	PRIMARY KEY (credentials_id),
-	FOREIGN KEY (credentials_id) REFERENCES person(credentials_id),
+	id            BIGINT NOT NULL,
+	label_id      BIGINT NOT NULL,
+	artistic_name VARCHAR(128) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES person(id),
 	FOREIGN KEY (label_id) REFERENCES label(id),
 	CHECK (length(artistic_name) > 0),
 	UNIQUE (artistic_name) --! REVIEW
 );
 
 CREATE TABLE consumer (
-	credentials_id	BIGSERIAL NOT NULL,
-	PRIMARY KEY (credentials_id),
-	FOREIGN KEY (credentials_id) REFERENCES person(credentials_id)
+	id BIGINT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES person(id)
 );
 
 CREATE TABLE song (
 	ismn      BIGSERIAL NOT NULL,
-	label_id  BIGSERIAL NOT NULL,
+	label_id  BIGINT NOT NULL,
 	genre     VARCHAR(16) NOT NULL,
 	title     VARCHAR(64) NOT NULL,
 	release   DATE NOT NULL,
@@ -111,27 +111,27 @@ CREATE TABLE playlist (
 
 CREATE TABLE prepaid_card ( --TODO
 	id          VARCHAR(16) NOT NULL,
-	consumer_id BIGSERIAL NOT NULL,
+	consumer_id BIGINT NOT NULL,
+	admin_id    BIGINT NOT NULL,
 	amount      SMALLINT NOT NULL DEFAULT 0,
 	limit_date  DATE NOT NULL,
-	admin_id    BIGSERIAL NOT NULL,
 	PRIMARY KEY (id, consumer_id),
-	FOREIGN KEY (consumer_id) REFERENCES consumer(credentials_id),
-	FOREIGN KEY (admin_id) REFERENCES administrator(credentials_id),
+	FOREIGN KEY (consumer_id) REFERENCES consumer(id),
+	FOREIGN KEY (admin_id) REFERENCES administrator(id),
 	CHECK (length(id) > 0)
 );
 
 CREATE TABLE comment (
-	id                BIGSERIAL NOT NULL,
-	content           TEXT NOT NULL,
-	comment_date      DATE NOT NULL,
-	song_ismn         BIGSERIAL NOT NULL,
-	consumer_id       BIGSERIAL NOT NULL,
-	parent_comment_id BIGSERIAL,
+	id            BIGSERIAL NOT NULL,
+	content       TEXT NOT NULL,
+	comment_date  DATE NOT NULL,
+	song_ismn     BIGINT NOT NULL,
+	consumer_id   BIGINT NOT NULL,
+	parent_id     BIGINT,
 	PRIMARY KEY (id),
 	FOREIGN KEY (song_ismn) REFERENCES song(ismn),
-	FOREIGN KEY (consumer_id) REFERENCES consumer(credentials_id),
-	FOREIGN KEY (parent_comment_id) REFERENCES comment(id),
+	FOREIGN KEY (consumer_id) REFERENCES consumer(id),
+	FOREIGN KEY (parent_id) REFERENCES comment(id),
 	CHECK (length(content) > 0)
 );
 
@@ -145,26 +145,26 @@ CREATE TABLE subscription ( --TODO
 );
 
 CREATE TABLE consumer_song (
-	views       SMALLINT NOT NULL DEFAULT 0,
+	song_ismn   BIGINT NOT NULL,
+	consumer_id BIGINT NOT NULL,
 	listen_date DATE NOT NULL,
-	song_ismn   BIGSERIAL NOT NULL,
-	consumer_id BIGSERIAL NOT NULL,
+	views       SMALLINT NOT NULL DEFAULT 0,
 	PRIMARY KEY (song_ismn, consumer_id),
 	FOREIGN KEY (song_ismn) REFERENCES song(ismn),
-	FOREIGN KEY (consumer_id) REFERENCES consumer(credentials_id)
-);
+	FOREIGN KEY (consumer_id) REFERENCES consumer(id)
+); -- index on listen_date
 
 CREATE TABLE consumer_subscription (
-	consumer_id     BIGSERIAL NOT NULL,
-	subscription_id BIGSERIAL NOT NULL,
+	consumer_id     BIGINT NOT NULL,
+	subscription_id BIGINT NOT NULL,
 	PRIMARY KEY (consumer_id, subscription_id),
-	FOREIGN KEY (consumer_id) REFERENCES consumer(credentials_id),
+	FOREIGN KEY (consumer_id) REFERENCES consumer(id),
 	FOREIGN KEY (subscription_id) REFERENCES subscription(id)
 );
 
 CREATE TABLE album_song (
-	album_id  BIGSERIAL NOT NULL,
-	song_ismn BIGSERIAL NOT NULL,
+	album_id  BIGINT NOT NULL,
+	song_ismn BIGINT NOT NULL,
 	PRIMARY KEY (album_id, song_ismn),
 	FOREIGN KEY (album_id) REFERENCES album(id),
 	FOREIGN KEY (song_ismn) REFERENCES song(ismn)
@@ -172,40 +172,40 @@ CREATE TABLE album_song (
 
 CREATE TABLE prepaid_card_subscription (
 	prepaid_card_id VARCHAR(16) NOT NULL,
-	consumer_id     BIGSERIAL NOT NULL,
-	subscription_id BIGSERIAL NOT NULL,
+	consumer_id     BIGINT NOT NULL,
+	subscription_id BIGINT NOT NULL,
 	PRIMARY KEY (prepaid_card_id, consumer_id, subscription_id),
 	FOREIGN KEY (prepaid_card_id, consumer_id) REFERENCES prepaid_card(id, consumer_id),
 	FOREIGN KEY (subscription_id) REFERENCES subscription(id)
 );
 
 CREATE TABLE album_artist (
-	album_id  BIGSERIAL,
-	artist_id BIGSERIAL,
+	album_id  BIGINT,
+	artist_id BIGINT,
 	PRIMARY KEY (album_id, artist_id),
 	FOREIGN KEY (album_id) REFERENCES album(id),
-	FOREIGN KEY (artist_id) REFERENCES artist(credentials_id)
+	FOREIGN KEY (artist_id) REFERENCES artist(id)
 );
 
 CREATE TABLE song_artist (
-	song_ismn BIGSERIAL,
-	artist_id BIGSERIAL,
+	song_ismn BIGINT,
+	artist_id BIGINT,
 	PRIMARY KEY (song_ismn, artist_id),
 	FOREIGN KEY (song_ismn) REFERENCES song(ismn),
-	FOREIGN KEY (artist_id) REFERENCES artist(credentials_id)
+	FOREIGN KEY (artist_id) REFERENCES artist(id)
 );
 
 CREATE TABLE consumer_playlist (
-	consumer_id BIGSERIAL,
-	playlist_id BIGSERIAL,
+	consumer_id BIGINT,
+	playlist_id BIGINT,
 	PRIMARY KEY (consumer_id, playlist_id),
-	FOREIGN KEY (consumer_id) REFERENCES consumer(credentials_id),
+	FOREIGN KEY (consumer_id) REFERENCES consumer(id),
 	FOREIGN KEY (playlist_id) REFERENCES playlist(id)
 );
 
 CREATE TABLE song_playlist (
-	song_ismn   BIGSERIAL,
-	playlist_id BIGSERIAL,
+	song_ismn   BIGINT,
+	playlist_id BIGINT,
 	PRIMARY KEY (song_ismn, playlist_id),
 	FOREIGN KEY (song_ismn) REFERENCES song(ismn),
 	FOREIGN KEY (playlist_id) REFERENCES playlist(id)
